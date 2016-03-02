512c2ae7484bc6f1ecfb55093be2d38166876cf2
陈磊 <chenlei@chenleideMacBook-Pro.local>
2016-03-03 00:52:39 +0800

    up

--- app/src/main/java/com/filmresource/cn/fragment/BaseFragment.java
+++ app/src/main/java/com/filmresource/cn/fragment/BaseFragment.java
@@ -2,6 +2,7 @@ package com.filmresource.cn.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +13,16 @@ import com.filmresource.cn.bean.BaseUI;
 
 public class BaseFragment extends Fragment implements BaseUI {
 
+	protected FragmentActivity mContext;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mContext = this.getActivity();
+	}
+
 	@Override
 	public void onUICallback(int type, Object object) {
-		
+
 	}
 }
